f8d718d6439ffbd1b95e233ab18219d908743d53
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-11-09 21:54:42 +0900

    GoodsSell vo 추가 + memberMapper

---
+++ banana/src/main/java/edu/kh/banana/member/model/vo/GoodsSell.java
@@ -0,0 +1,26 @@
+package edu.kh.banana.member.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+public class GoodsSell {
+
+	private int goodsNo;
+	private int sellerNo;
+	private int categoryNo;
+	private String title;
+	private int sellPrice;
+	private int viewCount;
+	private String description;
+	private String refreshedAt;
+	private String createdAt;
+	private String goodsDelFlag;
+	private String sellStatus;
+	
+}

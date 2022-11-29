a9cbcf057891a82f0f0a95ff8742c032cfbf1169
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-11-29 20:03:05 +0900

    log찍기 기능 삭제

--- banana/src/main/java/edu/kh/banana/common/aop/AfterAdviceAspect.java
+++ banana/src/main/java/edu/kh/banana/common/aop/AfterAdviceAspect.java
@@ -1,21 +1,21 @@
-package edu.kh.banana.common.aop;
-
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-@Component
-@Aspect
-public class AfterAdviceAspect {
-	
-	private Logger logger = LoggerFactory.getLogger(AfterAdviceAspect.class);
-	
-	@After("CommonPointcut.serviceImplPointcut()")
-	public void afterServiceLog(JoinPoint jp) {
-		logger.info("-----------------------------------------------");
-	}
-
-}
+//package edu.kh.banana.common.aop;
+//
+//import org.aspectj.lang.JoinPoint;
+//import org.aspectj.lang.annotation.After;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Aspect
+//public class AfterAdviceAspect {
+//	
+//	private Logger logger = LoggerFactory.getLogger(AfterAdviceAspect.class);
+//	
+//	@After("CommonPointcut.serviceImplPointcut()")
+//	public void afterServiceLog(JoinPoint jp) {
+//		logger.info("-----------------------------------------------");
+//	}
+//
+//}

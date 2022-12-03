f3a429fc0f2e640060d3d94b5a388345844c63ae
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-12-03 12:21:12 +0900

    바나나페이 js 수정

--- banana/src/main/webapp/resources/js/pay/bananaPay.js
+++ banana/src/main/webapp/resources/js/pay/bananaPay.js
@@ -19,29 +19,29 @@ function openPop() {
 }
 
 
-document.getElementById("chargeBtn").addEventListener("click", function(){
+document.getElementById("chargeBtn").addEventListener("click", function () {
     payCharge.style.display = "block";
     payRefund.style.display = "none";
     payLog.style.display = "none";
 });
-document.getElementById("refundBtn").addEventListener("click", function(){
+document.getElementById("refundBtn").addEventListener("click", function () {
     payRefund.style.display = "block";
     payCharge.style.display = "none";
     payLog.style.display = "none";
 });
-document.getElementById("logBtn").addEventListener("click", function(){
+document.getElementById("logBtn").addEventListener("click", function () {
     payLog.style.display = "block";
     payCharge.style.display = "none";
     payRefund.style.display = "none";
 });
 
-document.querySelector(".closeCharge").addEventListener("click", function(){
+document.querySelector(".closeCharge").addEventListener("click", function () {
     payCharge.style.display = "none";
 });
-document.querySelector(".closeRefund").addEventListener("click", function(){
+document.querySelector(".closeRefund").addEventListener("click", function () {
     payRefund.style.display = "none";
 });
-document.querySelector(".closeLog").addEventListener("click", function(){
+document.querySelector(".closeLog").addEventListener("click", function () {
     payLog.style.display = "none";
 });
 
@@ -60,118 +60,80 @@ refundPrice.value = Number(0);
 
 
 // if(payCharge.style.display != "none"){
-    
-    document.getElementsByClassName("charge1")[0].addEventListener("click", function(){
-        chargePrice.value = Number(chargePrice.value) + Number(10000);
-    });
-    document.getElementsByClassName("charge5")[0].addEventListener("click", function(){
-        chargePrice.value = Number(chargePrice.value) + Number(50000);
-    });
-    document.getElementsByClassName("charge10")[0].addEventListener("click", function(){
-        chargePrice.value = Number(chargePrice.value) + Number(100000);
-    });
+
+document.getElementsByClassName("charge1")[0].addEventListener("click", function () {
+    chargePrice.value = Number(chargePrice.value) + Number(10000);
+    chargeValidate();
+});
+document.getElementsByClassName("charge5")[0].addEventListener("click", function () {
+    chargePrice.value = Number(chargePrice.value) + Number(50000);
+    chargeValidate();
+});
+document.getElementsByClassName("charge10")[0].addEventListener("click", function () {
+    chargePrice.value = Number(chargePrice.value) + Number(100000);
+    chargeValidate();
+});
 // }
 
 // if(payRefund.style.display != "none"){
-    
-    document.getElementsByClassName("charge1")[1].addEventListener("click", function(){
-        refundPrice.value = Number(refundPrice.value) + Number(10000);
-    });
-    document.getElementsByClassName("charge5")[1].addEventListener("click", function(){
-        refundPrice.value = Number(refundPrice.value) + Number(50000);
-    });
-    document.getElementsByClassName("charge10")[1].addEventListener("click", function(){
-        refundPrice.value = Number(refundPrice.value) + Number(100000);
-    });
-    
-    document.getElementById("chargeAll").addEventListener("click", function(){    
-        refundPrice.value = payRemain.innerText;
-    });
+
+document.getElementsByClassName("charge1")[1].addEventListener("click", function () {
+    refundPrice.value = Number(refundPrice.value) + Number(10000);
+    refundValidate();
+});
+document.getElementsByClassName("charge5")[1].addEventListener("click", function () {
+    refundPrice.value = Number(refundPrice.value) + Number(50000);
+    refundValidate();
+});
+document.getElementsByClassName("charge10")[1].addEventListener("click", function () {
+    refundPrice.value = Number(refundPrice.value) + Number(100000);
+    refundValidate();
+});
+
+document.getElementById("chargeAll").addEventListener("click", function () {
+    refundPrice.value = payRemain.innerText;
+    refundValidate();
+});
 // }
 
 
 
 // 금액 초기화
 const resetPrice = document.getElementsByClassName("resetPrice");
-resetPrice[0].addEventListener("click", function(){
+resetPrice[0].addEventListener("click", function () {
     chargePrice.value = 0;
+    chargeValidate();
 });
-resetPrice[1].addEventListener("click", function(){
+resetPrice[1].addEventListener("click", function () {
     refundPrice.value = 0;
+    refundValidate();
 });
 
 
 
 // 직접입력 input버튼에 콤마 구현, 유효성 검사
-refundPrice.addEventListener("input", function(){
+refundPrice.addEventListener("input", function () {
+    refundValidate();
+});
 
-
-
-    const refundMessage = document.getElementById("refundMessage");
-
-    if(refundPrice.value.trim().length == 0){
-        refundMessage.innerText = "숫자만 입력해주세요";
-        refundMessage.value = 0;
-        refundMessage.classList.remove("error", "confirm");
-
-        refundFlag = false;
-        return;
-    }
-
-    const regEx = /^[0-9]{1,7}$/;
-    const regEx2 = /^[0-9]{8,}$/;
-
-
-
-    if(regEx.test(refundPrice.value)){ // 유효한 경우
-
-        refundPrice.value = Number(refundPrice.value);
-
-        refundMessage.innerText = "유효한 형식입니다(숫자)";
-        refundMessage.classList.remove("error");
-        refundMessage.classList.add("confirm");
-
-        refundFlag = true;
-
-
-    } else if (regEx2.test(refundPrice.value)) {
-
-        refundPrice.value = Number(refundPrice.value);
-
-        refundMessage.innerText = "입력한 금액이 너무 큽니다";
-        refundMessage.classList.add("error");
-        refundMessage.classList.remove("confirm");
-
-        refundFlag = false;
-        return;
-
-    } else {
-        refundMessage.innerText = "";
-        refundMessage.innerText = "숫자만 입력해주세요";
-        refundMessage.classList.add("error");
-        refundMessage.classList.remove("confirm");
-
-        refundFlag = false;
-        return;
-    }
-}
-);
-
-chargePrice.addEventListener("focusin", ()=>{
-    if(chargePrice.value == 0) {
+chargePrice.addEventListener("focusin", () => {
+    if (chargePrice.value == 0) {
         chargePrice.value = "";
     }
 });
 
 
 
-chargePrice.addEventListener("input", function(){
+chargePrice.addEventListener("input", function () {
+    chargeValidate();
+});
 
 
-
+// 충전 유효성 검사
+const chargeValidate = () => {
     const chargeMessage = document.getElementById("chargeMessage");
 
-    if(chargePrice.value.trim().length == 0){
+    if (chargePrice.value.trim().length == 0 || chargePrice.value == 0) {
         chargeMessage.innerText = "숫자만 입력해주세요";
         chargeMessage.value = 0;
         chargeMessage.classList.remove("error", "confirm");
@@ -185,7 +147,7 @@ chargePrice.addEventListener("input", function(){
 
 
 
-    if(regEx.test(chargePrice.value)){ // 유효한 경우
+    if (regEx.test(chargePrice.value)) { // 유효한 경우
 
         chargePrice.value = Number(chargePrice.value);
 
@@ -216,29 +178,75 @@ chargePrice.addEventListener("input", function(){
         chargeFlag = false;
         return;
     }
-}
-);
+};
+
+
+// 환불 유효성 검사
+const refundValidate = () => {
+
+    const refundMessage = document.getElementById("refundMessage");
+
+    if (refundPrice.value.trim().length == 0 || refundPrice.value == 0) {
+        refundMessage.innerText = "숫자만 입력해주세요";
+        refundMessage.value = 0;
+        refundMessage.classList.remove("error", "confirm");
+
+        refundFlag = false;
+        return;
+    }
+
+    const regEx = /^[0-9]{1,7}$/;
+    const regEx2 = /^[0-9]{8,}$/;
 
 
 
+    if (regEx.test(refundPrice.value)) { // 유효한 경우
+
+        refundPrice.value = Number(refundPrice.value);
+
+        refundMessage.innerText = "유효한 형식입니다(숫자)";
+        refundMessage.classList.remove("error");
+        refundMessage.classList.add("confirm");
+
+        refundFlag = true;
 
 
+    } else if (regEx2.test(refundPrice.value)) {
 
+        refundPrice.value = Number(refundPrice.value);
+
+        refundMessage.innerText = "입력한 금액이 너무 큽니다";
+        refundMessage.classList.add("error");
+        refundMessage.classList.remove("confirm");
+
+        refundFlag = false;
+        return;
+
+    } else {
+        refundMessage.innerText = "";
+        refundMessage.innerText = "숫자만 입력해주세요";
+        refundMessage.classList.add("error");
+        refundMessage.classList.remove("confirm");
+
+        refundFlag = false;
+        return;
+    }
+};
 
 
 //충전하기
-document.getElementById("chargeSubmitBtn").addEventListener("click", function(e){
+document.getElementById("chargeSubmitBtn").addEventListener("click", function (e) {
 
-    if(!chargeFlag){
+    if (!chargeFlag) {
 
-        if(chargePrice.value == 0){
+        if (chargePrice.value == 0) {
             alert("0원을 충전할 수 없습니다. 금액을 입력해주세요");
             chargePrice.value = Number(0);
             chargePrice.focus();
             e.preventDefault();
             return;
         }
-        if(chargePrice.value % 10000 != 0){
+        if (chargePrice.value % 10000 != 0) {
             alert("충전할 금액을 만 원 단위로 입력해주세요");
             chargePrice.value = Number(0);
             chargePrice.focus();
@@ -246,21 +254,21 @@ document.getElementById("chargeSubmitBtn").addEventListener("click", function(e)
             return;
         }
         return;
-    } 
+    }
 
 });
 
 // 환불하기
-document.getElementById("refundSubmitBtn").addEventListener("click", function(e){
+document.getElementById("refundSubmitBtn").addEventListener("click", function (e) {
 
-    if(refundPrice.value == 0){
+    if (refundPrice.value == 0) {
         alert("0원을 환불할 수 없습니다. 환불할 금액을 입력해주세요");
         refundPrice.value = Number(0);
         refundPrice.focus();
         e.preventDefault();
         return;
     }
-    if(refundPrice.value - payRemain.innerText > 0){
+    if (refundPrice.value - payRemain.innerText > 0) {
         alert("현재 잔액보다 더 큰 금액을 환불할 수 없습니다.");
         refundPrice.value = Number(0);
         refundPrice.focus();
@@ -281,8 +289,8 @@ const selectYear = document.getElementById("selectYear");
 
 
 
-for(let month of dropdownContent2){
-    if(Number(month.innerText.substring(0,2)) == new Date().getMonth()+1){
+for (let month of dropdownContent2) {
+    if (Number(month.innerText.substring(0, 2)) == new Date().getMonth() + 1) {
         month.classList.add("selected");
         selectMonth.innerText = month.innerText;
     }
@@ -291,8 +299,8 @@ for(let month of dropdownContent2){
 
 
 
-for(let year of dropdownContent1){
-    if(Number(year.innerText.substring(0,4)) == new Date().getFullYear()){
+for (let year of dropdownContent1) {
+    if (Number(year.innerText.substring(0, 4)) == new Date().getFullYear()) {
         year.classList.add("selectedYear");
         selectYear.innerText = year.innerText;
     }
@@ -302,22 +310,22 @@ for(let year of dropdownContent1){
 
 
 
-document.querySelector(".dropdown-content2").addEventListener("mouseover",()=>{
+document.querySelector(".dropdown-content2").addEventListener("mouseover", () => {
     selectMonth.nextElementSibling.style.display = "block";
     selectYear.nextElementSibling.style.display = "none";
 });
 
-selectMonth.addEventListener("mouseenter",()=>{
+selectMonth.addEventListener("mouseenter", () => {
     selectMonth.nextElementSibling.style.display = "block";
     selectYear.nextElementSibling.style.display = "none";
 });
 
-for(let month of dropdownContent2){
-    month.addEventListener("mouseleave",()=>{
+for (let month of dropdownContent2) {
+    month.addEventListener("mouseleave", () => {
         selectYear.nextElementSibling.style.display = "none";
         selectMonth.nextElementSibling.style.display = "none";
     })
-    month.addEventListener("click", ()=>{
+    month.addEventListener("click", () => {
         selectMonth.innerText = month.innerText;
         selectMonth.nextElementSibling.style.display = "none";
 
@@ -331,22 +339,22 @@ for(let month of dropdownContent2){
 
 
 
-document.querySelector(".dropdown-content1").addEventListener("mouseover",()=>{
+document.querySelector(".dropdown-content1").addEventListener("mouseover", () => {
     selectYear.nextElementSibling.style.display = "block";
     selectMonth.nextElementSibling.style.display = "none";
 })
 
-selectYear.addEventListener("mouseenter",()=>{
+selectYear.addEventListener("mouseenter", () => {
     selectYear.nextElementSibling.style.display = "block";
     selectMonth.nextElementSibling.style.display = "none";
 })
 
-for(let year of dropdownContent1){
-    year.addEventListener("mouseleave",()=>{
+for (let year of dropdownContent1) {
+    year.addEventListener("mouseleave", () => {
         selectYear.nextElementSibling.style.display = "none";
         selectMonth.nextElementSibling.style.display = "none";
     })
-    year.addEventListener("click", ()=>{
+    year.addEventListener("click", () => {
         selectYear.innerText = year.innerText;
         selectYear.nextElementSibling.style.display = "none";
     });
@@ -354,13 +362,13 @@ for(let year of dropdownContent1){
 }
 
 
-document.getElementById("monthBefore").addEventListener("click", ()=>{
+document.getElementById("monthBefore").addEventListener("click", () => {
     const selectMonth = document.getElementById("selectMonth");
-    if(selectMonth.innerText == "01월"){
-        if(selectYear.innerText == "2022년"){
+    if (selectMonth.innerText == "01월") {
+        if (selectYear.innerText == "2022년") {
             alert("바나나마켓이 출시한 연도보다 이전 날짜를 조회할 수 없어요~ ^^*");
         } else {
-            selectYear.innerText = (Number(selectYear.innerText.substring(0,4))-1) + "년";
+            selectYear.innerText = (Number(selectYear.innerText.substring(0, 4)) - 1) + "년";
             selectMonth.classList.remove("selected");
             document.getElementById("month12").classList.add("selected");
             selectMonth.innerText = "12월";
@@ -370,18 +378,18 @@ document.getElementById("monthBefore").addEventListener("click", ()=>{
         el.classList.toggle("selected");
         el.previousElementSibling.classList.toggle("selected");
         selectMonth.innerText = el.previousElementSibling.innerText;
-        
+
     }
 
 });
 
-document.getElementById("monthAfter").addEventListener("click", ()=>{
+document.getElementById("monthAfter").addEventListener("click", () => {
     const selectMonth = document.getElementById("selectMonth");
-    if(selectMonth.innerText == "12월"){
-        if(selectYear.innerText == "2024년"){
+    if (selectMonth.innerText == "12월") {
+        if (selectYear.innerText == "2024년") {
             alert("조회할 수 없습니다");
         } else {
-            selectYear.innerText = (Number(selectYear.innerText.substring(0,4))+1) + "년";
+            selectYear.innerText = (Number(selectYear.innerText.substring(0, 4)) + 1) + "년";
             selectMonth.classList.remove("selected");
             document.getElementById("month1").classList.add("selected");
             selectMonth.innerText = "01월";
@@ -391,7 +399,7 @@ document.getElementById("monthAfter").addEventListener("click", ()=>{
         el.classList.toggle("selected");
         el.nextElementSibling.classList.toggle("selected");
         selectMonth.innerText = el.nextElementSibling.innerText;
-        
+
     }
 
 });
@@ -403,19 +411,19 @@ const selectPayCtg = document.getElementsByClassName("payCtg2__div");
 
 selectPayCtg[0].classList.add("ctgChecked");
 
-selectPayCtg[0].addEventListener("click", ()=>{
+selectPayCtg[0].addEventListener("click", () => {
     selectPayCtg[0].classList.add("ctgChecked");
     selectPayCtg[1].classList.remove("ctgChecked");
     selectPayCtg[2].classList.remove("ctgChecked");
 });
 
-selectPayCtg[1].addEventListener("click", ()=>{
+selectPayCtg[1].addEventListener("click", () => {
     selectPayCtg[0].classList.remove("ctgChecked");
     selectPayCtg[1].classList.add("ctgChecked");
     selectPayCtg[2].classList.remove("ctgChecked");
 });
 
-selectPayCtg[2].addEventListener("click", ()=>{
+selectPayCtg[2].addEventListener("click", () => {
     selectPayCtg[0].classList.remove("ctgChecked");
     selectPayCtg[1].classList.remove("ctgChecked");
     selectPayCtg[2].classList.add("ctgChecked");
